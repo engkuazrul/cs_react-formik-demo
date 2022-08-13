8184b85a41c9e8d16608b3c117b069529a95b916
engkuazrul <engku@dormeo.my>
2022-08-14 00:03:46 +0800

    update

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, Field, ErrorMessage, FieldArray, FastField } from "formik";
 import * as Yup from "yup";
 import TextError from "./TextError";
 
@@ -11,9 +11,10 @@ const initialValues = {
   address: "",
   social: {
     facebook: "",
-    twitter: ""
+    twitter: "",
   },
-  phoneNumbers: ["", ""]
+  phoneNumbers: ["", ""],
+  phNumbers: [""],
 };
 
 const onSubmit = (values) => {
@@ -26,27 +27,36 @@ const validationSchema = Yup.object({
   channel: Yup.string().required("Required"),
 });
 
+const validatedComments = value => {
+  let error;
+
+  if(!value){
+    error = 'Required';
+  }
+
+  return error;
+}
+
 const YoutubeForm = () => {
   return (
     <Formik
       initialValues={initialValues}
       validationSchema={validationSchema}
       onSubmit={onSubmit}
+      validateOnChange={false}
     >
       <Form>
         <div className="form-control">
           <label htmlFor="name">Name</label>
           <Field type="text" id="name" name="name" />
-          <ErrorMessage name="name" component={TextError}/>
+          <ErrorMessage name="name" component={TextError} />
         </div>
 
         <div className="form-control">
           <label htmlFor="email">Email</label>
           <Field type="email" id="email" name="email" />
           <ErrorMessage name="email">
-            {
-              (errorMsg) =><div className="error">{errorMsg}</div>
-            }
+            {(errorMsg) => <div className="error">{errorMsg}</div>}
           </ErrorMessage>
         </div>
 
@@ -58,27 +68,28 @@ const YoutubeForm = () => {
             name="channel"
             placeholder="Youtube channel name"
           />
-          <ErrorMessage name="channel" component={TextError}/>
+          <ErrorMessage name="channel" component={TextError} />
         </div>
 
         <div className="form-control">
           <label htmlFor="comments">Comments</label>
-          <Field as="textarea" id="comments" name="comments" />
+          <Field as="textarea" id="comments" name="comments" validate={validatedComments}/>
+          <ErrorMessage name="comments" component={TextError} />
         </div>
 
         <div className="form-control">
           <label htmlFor="address">Address</label>
-          <Field name="address">
+          <FastField name="address">
             {(props) => {
               const { field, form, meta } = props;
               return (
                 <div>
-                  <input type='text' id="address" {...field} />
+                  <input type="text" id="address" {...field} />
                   {meta.touched && meta.error ? <div>{meta.error}</div> : null}
                 </div>
-              )
+              );
             }}
-          </Field>
+          </FastField>
         </div>
 
         <div className="form-control">
@@ -99,6 +110,36 @@ const YoutubeForm = () => {
         <div className="form-control">
           <label htmlFor="secondaryPh">Secondary phone number</label>
           <Field type="text" id="secondaryPh" name="phoneNumbers[1]" />
+        </div>
+
+        <div className="form-control">
+          <label>List of phone numbers</label>
+          <FieldArray
+            name="phNumbers"
+          >
+            {
+              (fieldArrayProps) => {
+                const {push, remove, form} = fieldArrayProps;
+                const {values} = form;
+                const {phNumbers} = values;
+                return <div>
+                  {
+                    phNumbers.map((phNumber, index) => (
+                      <div key={index}>
+                        <Field name={`phNumbers[${index}]`} />
+                        {
+                          index > 0 && (
+                            <button type="button" onClick={() => remove(index)}> - </button>
+                          )
+                        }
+                        <button type="button" onClick={() => push(index)}> + </button>
+                      </div>
+                    ))
+                  }
+                </div>
+              }
+            }
+          </FieldArray>
         </div>
 
         <button type="submit">Submit</button>
